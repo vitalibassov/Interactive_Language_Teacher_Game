c552ddeefd8da971cd2e4ea432cd5a6005cca61f
vitalibassov <vitaaas96@gmail.com>
2018-05-18 15:01:55 +0300

    Not added files.

--- android/assets/maps/textures/water_waves.tsx
+++ android/assets/maps/textures/water_waves.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="water" tilewidth="128" tileheight="64" tilecount="20" columns="5">
- <image source="water.png" width="640" height="256"/>
+<tileset name="water_waves" tilewidth="128" tileheight="64" tilecount="20" columns="5">
+ <image source="water_waves.png" width="640" height="256"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
