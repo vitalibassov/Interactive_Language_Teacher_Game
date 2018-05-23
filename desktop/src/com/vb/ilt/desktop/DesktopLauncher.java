1da22546e3424a31573aa7ece249b4c83c11fa39
vitalibassov <vitaaas96@gmail.com>
2018-05-23 18:10:13 +0300

    Tiny changes. It automatically sets a screen resolution according to a user's screen.

--- desktop/src/com/vb/ilt/desktop/DesktopLauncher.java
+++ desktop/src/com/vb/ilt/desktop/DesktopLauncher.java
@@ -1,16 +1,18 @@
 package com.vb.ilt.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.vb.ilt.InteractiveLangTeacherGame;
-import com.vb.ilt.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = (int)GameConfig.WIDTH;
-		config.height = (int)GameConfig.HEIGHT;
-		config.fullscreen = false;
+		config.setFromDisplayMode(displayMode);
+//		config.width = (int)GameConfig.WIDTH;
+//		config.height = (int)GameConfig.HEIGHT;
+//		config.fullscreen = false;
 		new LwjglApplication(new InteractiveLangTeacherGame(), config);
 	}
 }
